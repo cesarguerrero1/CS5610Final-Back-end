075bcb7a8950f38ed0625917a7265d55792ec694
Cesar Guerrero <cguerrero.cesar@gmail.com>
2022-12-15 01:26:52 -0500

    All Base Files for controllers, daos, and mongoose

--- controllers/AuthenticationController.js
+++ controllers/AuthenticationController.js
@@ -6,7 +6,16 @@
  * @file This controller will handle the authentication routes for our application
  */
 
+
+
 class AuthenticationController{
+
+    constructor(app){
+
+        //HTTP ENDPOINTS
+        
+    }
+
 
 }
 
